1e0b546f7ba3c94f283316f58fc6d6eefa9a6f09
Natalia <nataliapanchenko@ukr.net>
2026-03-12 10:30:44 +0100

    bug fixing

--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -1,13 +1,21 @@
 import dbConnect from "@/db/connect";
 import Event from "@/db/models/Events.js";
+import Category from "@/db/models/Categories";
+import Location from "@/db/models/Locations";
 
 export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
-    const events = await Event.find().populate(["category", "location"]);
-    return response.status(200).json(events);
-  } else {
-    return response.status(405).json({ message: "Method not allowed" });
+    try {
+      const events = await Event.find().populate(["category", "location"]);
+      return response.status(200).json(events);
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({
+        status: "Error",
+        message: error.message,
+      });
+    }
   }
 }
