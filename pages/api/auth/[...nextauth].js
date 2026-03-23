e57e8b59ab160a27358b440ac08b449d08963fe8
Natalia <nataliipanchenko@gmail.com>
2026-03-23 14:48:28 +0100

    add dummy user credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,21 +3,53 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/lib/db";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const isPreview = process.env.VERCEL_ENV === "preview";
 
 export const authOptions = {
-  adapter: MongoDBAdapter(clientPromise),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      allowDangerousEmailAccountLinking: true,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-      allowDangerousEmailAccountLinking: true,
-    }),
-  ],
+  adapter: isPreview ? undefined : MongoDBAdapter(clientPromise),
+
+  providers: isPreview
+    ? [
+        CredentialsProvider({
+          name: "credentials",
+          credentials: {
+            username: {
+              label: "Username",
+              type: "text",
+              placeholder: "username",
+            },
+            password: { label: "Password", type: "password" },
+          },
+          async authorize(credentials) {
+            if (
+              credentials.username === "fisch" &&
+              credentials.password === "fisch"
+            ) {
+              return {
+                id: "a1b2c3d4",
+                name: "Neuer Fisch",
+                email: "test@example.com",
+              };
+            }
+            return null;
+          },
+        }),
+      ]
+    : [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          allowDangerousEmailAccountLinking: true,
+        }),
+        GithubProvider({
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET,
+          allowDangerousEmailAccountLinking: true,
+        }),
+      ],
+
   callbacks: {
     async session({ session, user }) {
       if (session.user) {
